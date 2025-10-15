47c2dbd8d48585c0a749d5d00ea2a614c42511b6
Kasho-dev <kylegabrielarana@gmail.com>
2025-10-16 00:47:19 +0800

    - new pages - new buttons - added buttons to dashboard

---
+++ src/pages/analytics.tsx
@@ -0,0 +1,10 @@
+function Analytics() {
+    return (
+        <div>
+      <h1 className="text-2xl md:text-3xl font-bold">Analytics</h1>
+
+        </div>
+    )
+}
+
+export default Analytics;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,18 @@
-function dashboard() {
+import AddWallet from "../components/AddWallet";
+import AddTransaction from "../components/AddTransaction";
+import {CircleUserRound} from 'lucide-react'
+
+function Dashboard() {
   return (
     <div>
-      <h1 className="text-2xl md:text-3xl font-bold">Dashboard</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl md:text-3xl font-bold">Dashboard</h1>
+        <div className="flex gap-3">
+        <AddWallet />
+        <AddTransaction />
+        <CircleUserRound />
+        </div>
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         <div className="bg-[#34373D] rounded-md p-4 h-24"></div>
@@ -12,4 +23,4 @@ function dashboard() {
   );
 }
 
-export default dashboard;
+export default Dashboard;
